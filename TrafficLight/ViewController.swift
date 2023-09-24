00ce046fcb4c124ea44ae55984f9076ede96a6ff
deshollow <555demin@gmail.com>
2023-09-24 14:21:05 +0300

    HomeWork2 - done / 1st method

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,18 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var trafficLight: UIStackView!
     @IBOutlet weak var button: UIButton!
-    
+    /*1,2,3,4 - tags for ViewController elements, from top to button*/
     override func viewDidLoad() {
         super.viewDidLoad()
-        trafficLight.viewWithTag(1)?.alpha = 0.3
-        trafficLight.viewWithTag(2)?.alpha = 0.3
-        trafficLight.viewWithTag(3)?.alpha = 0.3
-        
+        trafficLight.viewWithTag(1)?.alpha = 0.5
+        trafficLight.viewWithTag(2)?.alpha = 0.5
+        trafficLight.viewWithTag(3)?.alpha = 0.5
+        button.setTitle("START", for: .normal)
     }
-
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        trafficLight.viewWithTag(1)?.layer.cornerRadius = (trafficLight.viewWithTag(1)?.frame.width ?? 0) / 2
+        trafficLight.viewWithTag(2)?.layer.cornerRadius = (trafficLight.viewWithTag(2)?.frame.width ?? 0) / 2
+        trafficLight.viewWithTag(3)?.layer.cornerRadius = (trafficLight.viewWithTag(3)?.frame.width ?? 0) / 2
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        button.setTitle("NEXT", for: .normal)
+        
+        if trafficLight.viewWithTag(1)?.alpha != 1 &&
+            trafficLight.viewWithTag(2)?.alpha != 1 {
+            trafficLight.viewWithTag(1)?.alpha = 1
+            trafficLight.viewWithTag(3)?.alpha = 0.5
+        } else if trafficLight.viewWithTag(1)?.alpha == 1 {
+            trafficLight.viewWithTag(1)?.alpha = 0.5
+            trafficLight.viewWithTag(2)?.alpha = 1
+        } else if trafficLight.viewWithTag(2)?.alpha == 1 {
+            trafficLight.viewWithTag(2)?.alpha = 0.5
+            trafficLight.viewWithTag(3)?.alpha = 1
+        }
+    }
 }
 
